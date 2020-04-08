9cd0102b93d0d1f05ea3de07fc0d16e124783211
Sagar Shah <sagar.shah@green-apex.com>
2020-04-08 11:56:16 +0530

    implemented pagination

--- src/containers/Posts/Posts.js
+++ src/containers/Posts/Posts.js
@@ -3,36 +3,54 @@ import axios from 'axios';
 
 import { Table } from 'react-bootstrap';
 import PostDetailsModal from '../../components/PostDetailsModal/PostDetailsModal';
+import Pagination from '../../components/Pagination/Pagination';
 
 export default class Posts extends Component {
     apiUrl = process.env.REACT_APP_API_URL;
+    intervalId;
 
     constructor(props) {
         super(props);
         this.state = {
             posts: [],
-            currentPage: 0,
+            currentActivePage: 0,
             totalPages: 0,
-            selectedPost: null
+            selectedPost: null,
+            selectedPage: -1
         };
     }
 
     componentDidMount() {
         this.getPosts();
+        this.intervalId = setInterval(() => {
+            this.getPosts();
+        }, 10000);
+    }
+
+    componentWillUnmount() {
+        // clear interval to prevent unnecessary memory leakage
+        if (this.intervalId) {
+            clearInterval(this.intervalId);
+        }
     }
 
     getPosts() {
-        axios.get(`${this.apiUrl}&page=${this.state.currentPage}`)
+        axios.get(`${this.apiUrl}&page=${this.state.currentActivePage}`)
             .then(response => {
                 if (response && response.status && response.status === 200) {
                     const data = response.data;
                     const oldPosts = [...this.state.posts];
                     const totalPages = this.state.totalPages + 1;
-                    const currentPage = totalPages;
+                    let currentActivePage = totalPages;
+                    const selectedPage = this.state.selectedPage;
+                    if (selectedPage > -1) {
+                        currentActivePage = selectedPage
+                    }
                     this.setState({
                         posts: [...oldPosts, ...data.hits],
-                        currentPage,
-                        totalPages
+                        currentActivePage,
+                        totalPages,
+                        selectedPage
                     });
                 }
             })
@@ -53,8 +71,21 @@ export default class Posts extends Component {
         });
     }
 
+    onPageChange(event) {
+        const target = event.target;
+        if (target && target.tagName.toLowerCase() === 'a') {
+            const selectedPage = parseInt(target.text.trim());
+            this.setState({
+                selectedPage,
+                currentActivePage: selectedPage
+            });
+        }
+    }
+
     render() {
-        const posts = this.state.posts;
+        const startIndex = this.state.selectedPage > -1 ? (this.state.selectedPage - 1) * 20 : (this.state.currentActivePage - 1) * 20;
+        const endIndex = startIndex + 20;
+        const posts = this.state.posts.slice(startIndex, endIndex);
         return (
             <React.Fragment>
                 <Table striped bordered hover>
@@ -79,7 +110,16 @@ export default class Posts extends Component {
                         })}
                     </tbody>
                 </Table>
-                {this.state.selectedPost ? <PostDetailsModal selectedPost={this.state.selectedPost} onModalClose={this.onModalClose.bind(this)} /> : null}
+
+                {/* Pagination */}
+                {this.state.currentActivePage ? (
+                    <Pagination currentActivePage={this.state.currentActivePage} totalPages={this.state.totalPages} onPageChange={this.onPageChange.bind(this)} />
+                ) : null}
+
+                {/* Post Modal */}
+                {this.state.selectedPost ? (
+                    <PostDetailsModal selectedPost={this.state.selectedPost} onModalClose={this.onModalClose.bind(this)} />
+                ) : null}
             </React.Fragment>
         )
     }
